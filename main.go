373fa3195146ddc7b225504862dcb71fac007408
Ben Frey <spartanhooah@gmail.com>
2021-10-12 09:21:51 -0400

    finish rock paper scissors

--- main.go
+++ main.go
@@ -19,29 +19,66 @@ const (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	playerChoice := ""
-	playerValue := -1
-
-	computerValue := rand.Intn(2)
-
 	reader := bufio.NewReader(os.Stdin)
-
 	clearScreen()
 
-	fmt.Print("Please enter rock, paper, or scissors ->\n")
-	playerChoice, _ = reader.ReadString('\n')
-	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+	for i := 1; i < 4; i++ {
+		fmt.Println("\nRound", i)
+		fmt.Println("--------")
+		fmt.Print("Please enter rock, paper, or scissors -> ")
+		playerChoice := ""
+		playerValue := -1
+		playerChoice, _ = reader.ReadString('\n')
+		playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+		computerValue := rand.Intn(2)
 
-	if playerChoice == "rock" {
-		playerValue = ROCK
-	} else if playerChoice == "paper" {
-		playerValue = PAPER
-	} else if playerChoice == "scissors" {
-		playerValue = SCISSORS
+		if playerChoice == "rock" {
+			playerValue = ROCK
+		} else if playerChoice == "paper" {
+			playerValue = PAPER
+		} else if playerChoice == "scissors" {
+			playerValue = SCISSORS
+		}
+
+		switch computerValue {
+		case ROCK:
+			fmt.Println("Computer chose ROCK")
+		case PAPER:
+			fmt.Println("Computer chose PAPER")
+		case SCISSORS:
+			fmt.Println("Computer chose SCISSORS")
+		default:
+		}
+
+		if playerValue == computerValue {
+			fmt.Println("It's a draw")
+			i--
+		} else {
+			switch playerValue {
+			case ROCK:
+				if computerValue == PAPER {
+					fmt.Println("Computer wins")
+				} else {
+					fmt.Println("Player wins")
+				}
+			case PAPER:
+				if computerValue == SCISSORS {
+					fmt.Println("Computer wins")
+				} else {
+					fmt.Println("Player wins")
+				}
+			case SCISSORS:
+				if computerValue == ROCK {
+					fmt.Println("Computer wins")
+				} else {
+					fmt.Println("Player wins")
+				}
+			default:
+				fmt.Println("Invalid choice")
+				i--
+			}
+		}
 	}
-
-	fmt.Println("Player chose", playerChoice, "and value is", playerValue)
-	fmt.Println("Computer chose", computerValue)
 }
 
 // clearScreen clears the screen
